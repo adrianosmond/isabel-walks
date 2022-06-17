4a895250dd82f58f9188fe088a909c543bddede5
Adrian Osmond <a.c.osmond@gmail.com>
2022-06-17 19:30:21 +0200

    Replace outdated Google Sheet library

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,4 +1,5 @@
-const Tabletop = require('tabletop');
+require('dotenv').config();
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 const fs = require('fs');
 
 const names = {
@@ -17,9 +18,7 @@ const names = {
   thames: 'Thames Path',
   wolds: 'Yorkshire Wolds Way',
 };
-
-const url =
-  'https://docs.google.com/spreadsheets/d/1dWCmkRkW6OsHkaDRLPDpYgt9Ov6Km1_6ZRu9Fw2vnSE/edit';
+/*
 
 const showInfo = (data) => {
   const trails = [...new Set(data.map((x) => x.trailid))];
@@ -37,17 +36,59 @@ const showInfo = (data) => {
             complete: segment.complete === 'TRUE',
           })),
       },
+
+*/
+
+const columns = ['trailid', 'from', 'to', 'distance', 'complete'];
+
+const fetchData = async () => {
+  const trails = [...new Set(Object.keys(names))];
+  const doc = new GoogleSpreadsheet(
+    '1dWCmkRkW6OsHkaDRLPDpYgt9Ov6Km1_6ZRu9Fw2vnSE',
+  );
+
+  let PRIVATE_KEY = process.env.REACT_APP_GOOGLE_PRIVATE_KEY || '';
+  PRIVATE_KEY = PRIVATE_KEY?.replace(/\\n/gm, '\n');
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
+    private_key: PRIVATE_KEY,
+  });
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+
+  const rowData = rows.map((r) =>
+    columns.map((c) => {
+      const v = r[c];
+      if (c === 'distance') return parseFloat(v);
+      if (c === 'complete') return v === 'TRUE';
+      return v;
+    }),
+  );
+
+  const trailData = trails.reduce(
+    (obj, id) => ({
+      ...obj,
+      [id]: {
+        name: names[id],
+        segments: rowData
+          .filter((x) => x[0] === id)
+          .map((segment) => ({
+            from: segment[1],
+            to: segment[2],
+            distance: segment[3],
+            complete: segment[4],
+          })),
+      },
     }),
     {},
   );
+
   fs.writeFileSync(
     './src/data/trails.json',
     JSON.stringify(trailData, null, 2),
   );
 };
 
-Tabletop.init({
-  key: url,
-  callback: showInfo,
-  simpleSheet: true,
-});
+fetchData();
